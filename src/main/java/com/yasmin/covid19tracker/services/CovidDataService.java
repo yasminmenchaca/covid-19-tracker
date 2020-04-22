b1adaad4189f91ce09cb0dc37a1a6c5c3662b9b8
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-22 09:57:00 -0500

    in progress

--- src/main/java/com/yasmin/covid19tracker/services/CovidDataService.java
+++ src/main/java/com/yasmin/covid19tracker/services/CovidDataService.java
@@ -1,9 +1,13 @@
 package com.yasmin.covid19tracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,6 +19,7 @@ public class CovidDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
+    @Scheduled(cron = "* * * * * *") // running method every second
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -22,6 +27,16 @@ public class CovidDataService {
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(httpResponse.body());
+//        System.out.println(httpResponse.body());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+
+        //Header auto detection
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+
+        }
     }
 }
