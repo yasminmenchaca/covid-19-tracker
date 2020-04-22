61207bc59c901b6c711b81930a5625d2f1674e5c
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-22 12:25:33 -0500

    difference from last day to today

--- src/main/java/com/yasmin/covid19tracker/services/CovidDataService.java
+++ src/main/java/com/yasmin/covid19tracker/services/CovidDataService.java
@@ -47,8 +47,12 @@ public class CovidDataService {
             LocationStats locationStat = new LocationStats();
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
-            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
-//            System.out.println(locationStat);
+
+            int latestCases = Integer.parseInt(record.get(record.size() - 1));
+            int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+
             newStats.add(locationStat);
         }
         this.allStats = newStats;
