d1ea4d1b5ab89491fe6fd42fb9a3ea6c467d8451
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-22 12:25:53 -0500

    added model attributes

--- src/main/java/com/yasmin/covid19tracker/controllers/HomeController.java
+++ src/main/java/com/yasmin/covid19tracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.yasmin.covid19tracker.controllers;
 
+import com.yasmin.covid19tracker.models.LocationStats;
 import com.yasmin.covid19tracker.services.CovidDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,16 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationStats", covidDataService.getAllStats());
+        List<LocationStats> allStats = covidDataService.getAllStats();
+
+        int totalReportedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPrevDay).sum();
+
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
         return "home";
     }
 }
