ada33006dd8ffb97143ce950fd5217833f47874b
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-22 11:33:21 -0500

    grabbing data

--- src/main/java/com/yasmin/covid19tracker/services/CovidDataService.java
+++ src/main/java/com/yasmin/covid19tracker/services/CovidDataService.java
@@ -1,5 +1,6 @@
 package com.yasmin.covid19tracker.services;
 
+import com.yasmin.covid19tracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,31 +13,44 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CovidDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
-    @Scheduled(cron = "* * * * * *") // running method every second
+    @Scheduled(cron = "* * 1 * * *") // running method every day
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
-        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-//        System.out.println(httpResponse.body());
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         StringReader csvBodyReader = new StringReader(httpResponse.body());
 
         //Header auto detection
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
-
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+//            System.out.println(locationStat);
+            newStats.add(locationStat);
         }
+        this.allStats = newStats;
     }
 }
