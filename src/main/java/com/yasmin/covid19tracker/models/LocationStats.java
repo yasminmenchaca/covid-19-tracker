1326e605bc80db585be4633c5f78bee594b4bd4f
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-22 12:26:08 -0500

    added difference from prev day

--- src/main/java/com/yasmin/covid19tracker/models/LocationStats.java
+++ src/main/java/com/yasmin/covid19tracker/models/LocationStats.java
@@ -5,6 +5,7 @@ public class LocationStats {
     private String state;
     private String country;
     private int latestTotalCases;
+    private int diffFromPrevDay;
 
     public String getState() {
         return state;
@@ -28,6 +29,14 @@ public class LocationStats {
 
     public void setLatestTotalCases(int latestTotalCases) {
         this.latestTotalCases = latestTotalCases;
+    }
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
     }
 
     @Override
